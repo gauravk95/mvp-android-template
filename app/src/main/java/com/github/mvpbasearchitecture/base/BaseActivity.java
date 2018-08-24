281099837874d2cf681ce9a609d991dcda279e37
Gaurav Kumar <19gauravk95@gmail.com>
2018-08-25 00:00:35 +0530

    Added Apache 2.0 license

--- app/src/main/java/com/github/mvpbasearchitecture/base/BaseActivity.java
+++ app/src/main/java/com/github/mvpbasearchitecture/base/BaseActivity.java
@@ -1,3 +1,18 @@
+/*
+    Copyright 2018 Gaurav Kumar
+
+    Licensed under the Apache License, Version 2.0 (the "License");
+    you may not use this file except in compliance with the License.
+    You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+    Unless required by applicable law or agreed to in writing, software
+    distributed under the License is distributed on an "AS IS" BASIS,
+    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+    See the License for the specific language governing permissions and
+    limitations under the License.
+*/
 package com.github.mvpbasearchitecture.base;
 
 import android.content.Intent;
@@ -12,7 +27,7 @@ import com.github.mvpbasearchitecture.di.module.ActivityModule;
 /**
  * Base activity without any MVP component
  * Used to declare Activities that are dumb and only act as holder to other fragments
- *
+ * <p>
  * Created by gk
  */
 
@@ -43,5 +58,5 @@ public abstract class BaseActivity extends AppCompatActivity {
     public void onNewIntent(Intent intent) {
         this.setIntent(intent);
     }
-    
+
 }
